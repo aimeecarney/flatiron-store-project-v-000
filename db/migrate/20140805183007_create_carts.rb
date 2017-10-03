0ee6ca74a0da005e1fe0f4097d8cb8bee54df8a7
Aimee Carney <acarney29@gmail.com>
2017-10-03 02:28:16 +0000

    have all models passing except User

--- db/migrate/20140805183007_create_carts.rb
+++ db/migrate/20140805183007_create_carts.rb
@@ -1,5 +1,9 @@
 class CreateCarts < ActiveRecord::Migration
   def change
-
+    create_table :carts do |t|
+      t.float :total
+      t.integer :user_id
+      t.string :status, default: "not submitted"
+    end
   end
 end
